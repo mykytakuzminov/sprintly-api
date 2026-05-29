535ff793610f27eeebf4c332bbd6762d23f94a2b
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-05-29 14:21:29 +0200

    feat: add server with graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/mykytakuzminov/task-manager-api/internal/config"
 	"github.com/mykytakuzminov/task-manager-api/internal/repository/postgres"
+	"github.com/mykytakuzminov/task-manager-api/internal/server"
 )
 
 func main() {
@@ -21,4 +29,34 @@ func main() {
 		log.Fatalf("migrations failed: %v", err)
 	}
 	log.Println("migrations applied successfully")
+
+	router := chi.NewRouter()
+	server := server.New(cfg.Server.GetAddr(), router)
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- server.Run()
+	}()
+	log.Println("server is running")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-errCh:
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	case <-quit:
+		log.Println("shutting down server...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Fatalf("server shutdown failed: %v", err)
+		}
+	}
+
+	log.Println("server stopped")
 }
