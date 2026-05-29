ce372268a64e5925d0e41036acedf46d23707944
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-05-29 12:12:24 +0200

    feat: add task service

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -19,6 +19,21 @@ type Task struct {
 	UpdatedAt   time.Time
 }
 
+type CreateTaskInput struct {
+	AssigneeID  *uuid.UUID `validate:"omitempty"`
+	Name        string     `validate:"required,max=100"`
+	Description *string    `validate:"omitempty,max=500"`
+	DueDate     *time.Time `validate:"omitempty"`
+}
+
+type UpdateTaskInput struct {
+	ColumnID    uuid.UUID  `validate:"required"`
+	AssigneeID  *uuid.UUID `validate:"omitempty"`
+	Name        string     `validate:"required,max=100"`
+	Description *string    `validate:"omitempty,max=500"`
+	DueDate     *time.Time `validate:"omitempty"`
+}
+
 type TaskRepository interface {
 	Create(ctx context.Context, task *Task) error
 	GetByID(ctx context.Context, id uuid.UUID) (*Task, error)
@@ -27,4 +42,14 @@ type TaskRepository interface {
 	GetAllByAssigneeID(ctx context.Context, assigneeID uuid.UUID) ([]*Task, error)
 	Update(ctx context.Context, task *Task) error
 	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type TaskService interface {
+	Create(ctx context.Context, userID, columnID uuid.UUID, input *CreateTaskInput) (*Task, error)
+	GetByID(ctx context.Context, taskID uuid.UUID) (*Task, error)
+	GetAllByUserID(ctx context.Context, userID uuid.UUID) ([]*Task, error)
+	GetAllByColumnID(ctx context.Context, columnID uuid.UUID) ([]*Task, error)
+	GetAllByAssigneeID(ctx context.Context, assigneeID uuid.UUID) ([]*Task, error)
+	Update(ctx context.Context, taskID, userID uuid.UUID, input *UpdateTaskInput) error
+	Delete(ctx context.Context, taskID, userID uuid.UUID) error
 }
