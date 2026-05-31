1bf6b1dbd0e1610798edddd885cf79e5002700dd
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-05-31 13:33:18 +0200

    feat: add auth middleware (#18)

---
+++ internal/handler/middleware/auth.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/mykytakuzminov/task-manager-api/internal/auth"
+)
+
+func AuthMiddleware(a *auth.Auth) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			tokenString := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+			if tokenString == "" {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			userID, err := a.ParseToken(tokenString)
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), "userID", userID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
