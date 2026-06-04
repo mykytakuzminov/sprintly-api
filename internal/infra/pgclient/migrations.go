a7999177dfd0fbb10c05c8410b6c2ccd96d85f7e
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-06-04 19:06:57 +0200

    feat: setup docker-compose app service

--- internal/infra/pgclient/migrations.go
+++ internal/infra/pgclient/migrations.go
@@ -1,16 +1,26 @@
 package pgclient
 
 import (
+	"embed"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/mykytakuzminov/task-manager-api/internal/config"
 )
+
+//go:embed migrations
+var migrationsFS embed.FS
 
 func RunMigrations(cfg *config.Config) error {
 	url := cfg.Database.GetDSN()
 
-	m, err := migrate.New("file://migrations", url)
+	driver, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+    	return err
+	}
+
+	m, err := migrate.NewWithSourceInstance("iofs", driver, url)
 	if err != nil {
 		return err
 	}
