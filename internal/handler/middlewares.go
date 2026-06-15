43559ae857c1301e5897d38395ec8a7c7ed9cf97
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-06-15 14:18:49 +0200

    feat: add CORS middleware (#60)

--- internal/handler/middlewares.go
+++ internal/handler/middlewares.go
@@ -89,3 +89,18 @@ func RateLimiterMiddleware(svc domain.RateLimitService, logger *zap.SugaredLogge
 		})
 	}
 }
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
